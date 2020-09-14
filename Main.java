37c85a719645b41444cec2fd246eb0b226fb7124
bazzl1202 <bazzl1202@wrdsb.ca>
2020-09-14 16:39:20 +0000

    Now asks for coordinates and calculates slope.

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
  *
- * @author 
+ * @author Liana Bazzarella
  */
 public class Main {
 
@@ -10,6 +11,37 @@ public class Main {
    */
   public static void main(String[] args) {
     // TODO code application logic here
+    Scanner input = new Scanner(System.in);
+
+    // ask user for the first x coordinate
+    System.out.println("Please enter in the x coordinate of the first point");
+
+    // declare and intialize varaible that stores first x coordinate
+    double firstCoordinateX = input.nextDouble();
+
+    // ask user for the first y coordinate
+    System.out.println("Please enter in the y coordinate of the first point");
+
+    // declare and intialize varaible that stores first y coordinate
+    double firstCoordinateY = input.nextDouble();
+
+    // ask user for second coordinate x
+    System.out.println("Please enter in the x coordinate of the second point");
     
+    // declare and intialize varaible that stores second x coordinate
+    double secondCoordinateX = input.nextDouble();
+
+    // ask user for the second y coordinate
+    System.out.println("Please enter in the y coordinate of the second point");
+
+    // declare and intialize varaible that stores second y coordinate
+    double secondCoordinateY = input.nextDouble();
+
+    // declare and calculate slope
+    double slope = (secondCoordinateY-firstCoordinateY)/(secondCoordinateX-firstCoordinateX);
+
+    // tell the user the slope
+    System.out.println("The slope of your line would be " + slope + ".");
+
   }
 }
